19cbc2a70a7d8913f5a111f71552dd904726a107
awhamil <andrew.hamilton@gmail.com>
2023-04-19 11:49:28 -0400

    added endpoint for coco object detection

--- README.txt
+++ README.txt
@@ -1,4 +1,5 @@
 cp ~/Dev/data/nn_weights/yolov7-tiny-mrz.pt yolov7
+cp ~/Dev/data/nn_weights/yolov7.pt yolov7
 sudo docker build -f Dockerfile -t mrz .
 sudo docker run --rm --init -p 8181:8181 --name mrz mrz
 curl -v 0:8181/mrz -H "Content-Type:application/octet-stream" --data-binary @test_image.jpeg
--- yolov7/main.py
+++ yolov7/main.py
@@ -11,14 +11,38 @@ from flask import request
 
 app = Flask(__name__)
 
-model = custom(path_or_model='/app/yolov7/yolov7-tiny-mrz.pt') 
 
 @app.route("/ping")
 def ping():
     return {"success": True}
 
+@app.route('/coco', methods=["POST"])
+def coco():
+    model = custom(path_or_model='/app/yolov7/yolov7.pt')
+    img = Image.open(io.BytesIO(request.stream.read()))
+    raw_preds = model([img]).xyxy[0]
+    preds = []
+    names = [ 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+         'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+         'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+         'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
+         'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+         'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+         'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+         'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
+         'hair drier', 'toothbrush' ]
+    for raw_pred in raw_preds.tolist():
+        if raw_pred[4] >= 0.5:
+            preds.append({
+                'name': names[int(raw_pred[5])],
+                'bbox': [round(i) for i in raw_pred[0:4]],
+                'conf': raw_pred[4]
+            })
+    return preds
+
 @app.route("/mrz", methods=["POST"])
 def mrz():
+    model = custom(path_or_model='/app/yolov7/yolov7-tiny-mrz.pt') 
     img = Image.open(io.BytesIO(request.stream.read()))
     img_preds = model([img]).xyxy[0]
     if len(img_preds) > 0:
